208d2bc7bb2633f0df5d57d4bb8d4c9e4933502d
Mark Tikhonov <mtik.philosopher@gmail.com>
2024-02-04 13:01:28 -0800

    updating CLI to allow lowercase mode parameter inputs

--- src/cli.py
+++ src/cli.py
@@ -3,7 +3,7 @@ import argparse
 class CLI:
     def __init__(self):
         parser = argparse.ArgumentParser(description='Control a Raspberry Pi fan.')
-        parser.add_argument('--mode', type=str, choices=['ON', 'OFF', 'AUTO'], required=True, help='Fan mode')
+        parser.add_argument('--mode', type=str, choices=['ON', 'OFF', 'AUTO', 'on', 'off', 'auto'], required=True, help='Fan mode')
         parser.add_argument('--pin', type=int, default=23, help='GPIO pin connected to the fan')
         parser.add_argument('--critical_temperature', type=float, default=50.0, help='Temperature to turn the fan on')
         parser.add_argument('--minimum_temperature', type=float, default=30.0, help='Temperature to turn the fan off')
